cfb4918616664a63edbb664541c04c3430a482ff
Hoang Do <hoang2003ngolike@gmail.com>
2024-03-11 04:03:56 +0700

    completed feching multiple images from server

--- frontend/src/components/rightPanel.jsx
+++ frontend/src/components/rightPanel.jsx
@@ -26,108 +26,112 @@ const RightPanel = ({query, filters}) => {
         }
     }
 
+    // const handleClick = () => {        
+
+
+    //     const response2 = imageService.getImages("blah blah blah")
+    //     .then(
+    //     (response2) => {
+    //         // Convert the byte data to a base64-encoded string
+    //         var urls = response2.data['image_files']
+    //         var imageDataUrls = []
+
+    //         // for (var i = 0; i < urls.length; i++) {
+    //         for (var i = 0; i < 10; i++) {
+    //             var url = urls[i]
+    //             console.log("fetching " + i + "th url: " + url)
+
+    //             const response = imageService.getImage(url)
+    //             .then(
+    //             (response) => {
+    //                 // Convert the byte data to a base64-encoded string
+    //                 const base64ImageString = btoa(
+    //                     new Uint8Array(response.data).reduce(
+    //                     (data, byte) => data + String.fromCharCode(byte),
+    //                     ''
+    //                     )
+    //                 );
+
+    //                 // Create the data URL for the image
+    //                 const imageDataUrl = `data:image/jpeg;base64,${base64ImageString}`;
+
+    //                 imageDataUrls.push(
+    //                     { src: imageDataUrl, status: i }
+    //                 )
+    //             })
+    //             .catch((error) => {
+    //                 console.error('Error fetching image:', error);
+    //             });
+    //         }
+    //         setImages(imageDataUrls);
+    //     })
+    //     .catch((error) => {
+    //         console.error('Error fetching images:', error);
+    //     });
+
+    //     // const response = imageService.getImage("C:/Users/ADMIN/Downloads/unnamed.png")
+    //     // .then(
+    //     // (response) => {
+    //     //     console.log(response);
+    //     //     // Convert the byte data to a base64-encoded string
+    //     //     const base64ImageString = btoa(
+    //     //         new Uint8Array(response.data).reduce(
+    //     //         (data, byte) => data + String.fromCharCode(byte),
+    //     //         ''
+    //     //         )
+    //     //     );
+    // }
+
     const handleClick = () => {
-        
-        // imageService.getImages()
-        // .then(response => {
-        //     console.log(response);
-        //     const imageUrls = response.map(imageData => URL.createObjectURL(imageData));
-        //     setImages(imageUrls);
-        // })
-        // .catch(error => {
-        //     console.error('Error fetching images:', error);
-        // });
-
-        // const response = imageService.getImage("C:/Users/ADMIN/Downloads/unnamed.png")
-        // .then(
-        // (response) => {
-        //     console.log(response);
-        //     // Convert the byte data to a base64-encoded string
-        //     const base64ImageString = btoa(
-        //         new Uint8Array(response.data).reduce(
-        //         (data, byte) => data + String.fromCharCode(byte),
-        //         ''
-        //         )
-        //     );
-    const [image, setImage] = useState(null);
-    // const response = imageService.getImage("C:/Users/DELL/Pictures/8fd0ad7ae44f6a64c9016070.jpg")
-    // .then(
-    // (response) => {
-    //      // Convert the byte data to a base64-encoded string
-    //     const base64ImageString = btoa(
-    //         new Uint8Array(response.data).reduce(
-    //         (data, byte) => data + String.fromCharCode(byte),
-    //         ''
-    //         )
-    //     );
-
-    //     // Create the data URL for the image
-    //     const imageDataUrl = `data:image/jpeg;base64,${base64ImageString}`;
-
-    //     // Set the image data URL in the state
-    //     setImages([imageDataUrl]);
-    // })
-    // .catch((error) => {
-    //     console.error('Error fetching image:', error);
-    // });
-
-    const response2 = imageService.getImages("blah blah blah")
-    .then(
-    (response2) => {
-        // Convert the byte data to a base64-encoded string
-        var urls = response2.data['image_files']
-        var imageDataUrls = []
-
-        // for (var i = 0; i < urls.length; i++) {
-        for (var i = 0; i < 10; i++) {
-            var url = urls[i]
-            console.log("fetching " + i + "th url: " + url)
-
-            const response = imageService.getImage(url)
-            .then(
-            (response) => {
-                 // Convert the byte data to a base64-encoded string
-                const base64ImageString = btoa(
-                    new Uint8Array(response.data).reduce(
-                    (data, byte) => data + String.fromCharCode(byte),
-                    ''
-                    )
-                );
-
-                //     // Create the data URL for the image
-                //     const imageDataUrl = `data:image/jpeg;base64,${base64ImageString}`;
-
-                imageDataUrls.push(imageDataUrl)
-
-                // Set the image data URL in the state
-                
+        // First HTTP request
+        const response2 = imageService.getImages("blah blah blah")
+            .then((response2) => {
+                // Convert the byte data to a base64-encoded string
+                var urls = response2.data['image_files'];
+                var imageDataUrls = [];
+    
+                // Second HTTP request (sequentially inside the loop)
+                // You can use Promise.all() if you want to make requests concurrently
+                const fetchImages = (index) => {
+                    if (index < 50) {
+                        var url = urls[index];
+                        console.log("fetching " + index + "th url: " + url);
+    
+                        return imageService.getImage(url)
+                            .then((response) => {
+                                const base64ImageString = btoa(
+                                    new Uint8Array(response.data).reduce(
+                                        (data, byte) => data + String.fromCharCode(byte),
+                                        ''
+                                    )
+                                );
+    
+                                const imageDataUrl = `data:image/jpeg;base64,${base64ImageString}`;
+    
+                                // imageDataUrls.push({ src: imageDataUrl, status: index });
+                                imageDataUrls.push(imageDataUrl);
+    
+                                // Recursive call to fetch the next image
+                                return fetchImages(index + 1);
+                            })
+                            .catch((error) => {
+                                console.error('Error fetching image:', error);
+                            });
+                    } else {
+                        // All images fetched, set the state or do other operations
+                        setImages(imageDataUrls);
+                    }
+                };
+    
+                // Start fetching images from index 0
+                return fetchImages(0);
             })
             .catch((error) => {
-                console.error('Error fetching image:', error);
+                console.error('Error fetching images:', error);
             });
-        }
-        setImages(imageDataUrls);
-        
-        
-        // const base64ImageString = btoa(
-        //     new Uint8Array(response.data).reduce(
-        //     (data, byte) => data + String.fromCharCode(byte),
-        //     ''
-        //     )
-        // );
-
-        // Create the data URL for the image
-        // const imageDataUrl = `data:image/jpeg;base64,${base64ImageString}`;
-
-        //     // Set the image data URL in the state
-        // //     console.log(imageDataUrl);
-        //     setImages([imageDataUrl]);
-        // })
-        // .catch((error) => {
-        //     console.error('Error fetching images:', error);
-        // });
-    }
     
+        // You can add more code here or handle subsequent actions after the requests
+    }
 
 
     return(
