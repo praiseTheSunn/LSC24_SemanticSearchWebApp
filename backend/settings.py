c18173c6b70041c72fba3a0dc7de2b832aecd7ce
hdkhang6803 <hdkhang6803@gmail.com>
2024-03-11 11:09:48 +0700

    remove settings.py

--- backend/settings.py
+++
@@ -1,6 +0,0 @@
-keyframes_path = "H:\LSCDATA\Keyframes"
-
-clip_embedding_path = "H:/LSCDATA/embeddings/new_clip/clip"
-clip_model_path = "H:\LSCDATA\open_clip_pytorch_model.bin"
-clip_index_path = "H:\LSCDATA\clip.index"
-
