ce948e712bcd164c91b1235a344c26601ee3b2b8
minhquanBr2 <minhquanhole03@gmail.com>
2024-05-09 14:28:18 +0700

    change color on activity bar when clicked

--- frontend/src/pages/Home/home.jsx
+++ frontend/src/pages/Home/home.jsx
@@ -15,20 +15,56 @@ const LevelList = [
     { level: "Location", bg: TrapoziedBgGray3 },
 ];
 
+// create a list of image data (10 images needed)
+
+const imageList = [
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRY2oYj5Olj4XiuIB5uEeaWbxc8Y6_Zup5lcfEUCt5IIidsiHIUR_2xua7vepE7RP4KHCw&usqp=CAU",
+    "https://s1.ticketm.net/dam/a/a67/86eb84c0-ad6a-43c6-a55f-ff5d109c9a67_RETINA_PORTRAIT_3_2.jpg",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQANCqdY31uljrV73FJrq9RYPdHUbqqou5lXfTGDLH-dQ&s",
+    "https://assets.teenvogue.com/photos/641b2a23912ddccbabf80f80/16:9/w_2560%2Cc_limit/GettyImages-1474459622.jpg",
+    "https://m.media-amazon.com/images/I/71LVEINqZaL._UF1000,1000_QL80_.jpg",
+    "https://e3.365dm.com/23/08/1600x900/skynews-taylor-swift-santa-clara_6237922.jpg?20230802101540",
+    "https://ca-times.brightspotcdn.com/dims4/default/b598fb5/2147483647/strip/true/crop/4000x2667+0+0/resize/1200x800!/quality/75/?url=https%3A%2F%2Fcalifornia-times-brightspot.s3.amazonaws.com%2Fe0%2Fde%2F9e80ee1545d9bc32a14a304bede6%2Ftaylor-swift-francia-07405.jpg",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYFAXqcwl-tVJwjkdF3G3Fwry4ukTFtTNBcec4q2Hyfw&s",
+    "https://i9.ytimg.com/s_p/OLAK5uy_mvRgYPh8u-KBTbU6mkGyrg5CBduos_LEY/sddefault.jpg?sqp=COiP8bEGir7X7AMICKL-0LEGEAE=&rs=AOn4CLD1G-WpT3xymRoMXy3Dk4aUennlzg&v=1714700066",
+    "https://i.abcnewsfe.com/a/06f5ba1e-de2d-4b1a-b3df-4a1649608ac2/katy-perry-taylor-swift-01-ht-jt-240223_1708718542679_hpEmbed_4x5.jpg"
+]
+
+
+
 const timelineData = [
-    { date: "2023-01-01",score: 1, img_link: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRY2oYj5Olj4XiuIB5uEeaWbxc8Y6_Zup5lcfEUCt5IIidsiHIUR_2xua7vepE7RP4KHCw&usqp=CAU", location: "Location 1", activity: "Breakfast" },
-    { date: "2023-03-15",score: 2, img_link: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRY2oYj5Olj4XiuIB5uEeaWbxc8Y6_Zup5lcfEUCt5IIidsiHIUR_2xua7vepE7RP4KHCw&usqp=CAU", location: "Location 2", activity: "Drive to work"  },
-    { date: "2023-01-01",score: 3, img_link: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRY2oYj5Olj4XiuIB5uEeaWbxc8Y6_Zup5lcfEUCt5IIidsiHIUR_2xua7vepE7RP4KHCw&usqp=CAU", location: "Location 1", activity: "Lecturing"  },
-    { date: "2022-01-01",score: 1, img_link: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRY2oYj5Olj4XiuIB5uEeaWbxc8Y6_Zup5lcfEUCt5IIidsiHIUR_2xua7vepE7RP4KHCw&usqp=CAU", location: "Location 2", activity: "Breakfast"  },
-    { date: "2023-01-15",score: 3, img_link: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRY2oYj5Olj4XiuIB5uEeaWbxc8Y6_Zup5lcfEUCt5IIidsiHIUR_2xua7vepE7RP4KHCw&usqp=CAU", location: "Location 3", activity: "Breakfast"  },
-    { date: "2012-06-20",score: 3, img_link: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRY2oYj5Olj4XiuIB5uEeaWbxc8Y6_Zup5lcfEUCt5IIidsiHIUR_2xua7vepE7RP4KHCw&usqp=CAU", location: "Location 1", activity: "Breakfast"  },
-    { date: "2022-01-01",score: 2, img_link: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRY2oYj5Olj4XiuIB5uEeaWbxc8Y6_Zup5lcfEUCt5IIidsiHIUR_2xua7vepE7RP4KHCw&usqp=CAU", location: "Location 2", activity: "Drive to work"  },
-    { date: "2023-12-15",score: 1, img_link: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRY2oYj5Olj4XiuIB5uEeaWbxc8Y6_Zup5lcfEUCt5IIidsiHIUR_2xua7vepE7RP4KHCw&usqp=CAU", location: "Location 3", activity: "Breakfast"  },
-    { date: "2023-01-01",score: 2, img_link: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRY2oYj5Olj4XiuIB5uEeaWbxc8Y6_Zup5lcfEUCt5IIidsiHIUR_2xua7vepE7RP4KHCw&usqp=CAU", location: "Location 4", activity: "Breakfast"  },
-    { date: "2023-01-01",score: 3, img_link: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRY2oYj5Olj4XiuIB5uEeaWbxc8Y6_Zup5lcfEUCt5IIidsiHIUR_2xua7vepE7RP4KHCw&usqp=CAU", location: "Location 1", activity: "Drive to work"  },
-    { date: "2023-01-01",score: 3, img_link: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRY2oYj5Olj4XiuIB5uEeaWbxc8Y6_Zup5lcfEUCt5IIidsiHIUR_2xua7vepE7RP4KHCw&usqp=CAU", location: "Location 2", activity: "Drive to work"  },
-    { date: "2023-01-01",score: 2, img_link: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRY2oYj5Olj4XiuIB5uEeaWbxc8Y6_Zup5lcfEUCt5IIidsiHIUR_2xua7vepE7RP4KHCw&usqp=CAU", location: "Location 2", activity: "Lecturing"  },
-    { date: "2023-01-01",score: 1, img_link: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRY2oYj5Olj4XiuIB5uEeaWbxc8Y6_Zup5lcfEUCt5IIidsiHIUR_2xua7vepE7RP4KHCw&usqp=CAU", location: "Location 2", activity: "Lecturing"  },
+    { date: "2023-01-01", score: 7, img_link: imageList[0], location: "Location 1", activity: "Dancing" },
+    { date: "2023-03-15", score: 9, img_link: imageList[1], location: "Location 2", activity: "Drive to work"  },
+    { date: "2023-01-01", score: 3, img_link: imageList[4], location: "Location 1", activity: "Lecturing"  },
+    { date: "2022-01-01", score: 1, img_link: imageList[8], location: "Location 2", activity: "Breakfast"  },
+    { date: "2023-01-15", score: 3, img_link: imageList[2], location: "Location 3", activity: "Breakfast"  },
+    { date: "2012-06-20", score: 3, img_link: imageList[3], location: "Location 1", activity: "Dancing"  },
+    { date: "2022-01-01", score: 8, img_link: imageList[4], location: "Location 2", activity: "Drive to work"  },
+    { date: "2023-12-15", score: 4, img_link: imageList[5], location: "Location 3", activity: "Breakfast"  },
+    { date: "2023-01-01", score: 2, img_link: imageList[2], location: "Location 1", activity: "Breakfast"  },
+    { date: "2023-01-01", score: 5, img_link: imageList[3], location: "Location 1", activity: "Drive to work"  },
+    { date: "2023-01-01", score: 3, img_link: imageList[9], location: "Location 2", activity: "Drive to work"  },
+    { date: "2023-01-01", score: 2, img_link: imageList[2], location: "Location 3", activity: "Lecturing"  },
+    { date: "2023-01-02", score: 0, img_link: imageList[9], location: "Location 3", activity: "Lecturing"  },
+    { date: "2012-06-20", score: 4, img_link: imageList[5], location: "Location 1", activity: "Dancing"  },
+    { date: "2022-01-01", score: 8, img_link: imageList[0], location: "Location 2", activity: "Breakfast"  },
+    { date: "2023-12-15", score: 4, img_link: imageList[1], location: "Location 2", activity: "Breakfast"  },
+    { date: "2023-01-01", score: 2, img_link: imageList[5], location: "Location 4", activity: "Drive to work"  },
+    { date: "2023-03-15", score: 8, img_link: imageList[2], location: "Location 4", activity: "Lecturing"  },
+    { date: "2023-03-15", score: 5, img_link: imageList[3], location: "Location 4", activity: "Drive to work"  },
+    { date: "2023-01-02", score: 3, img_link: imageList[7], location: "Location 1", activity: "Drive to work"  },
+    { date: "2022-01-02", score: 1, img_link: imageList[8], location: "Location 2", activity: "Breakfast"  },
+    { date: "2022-01-02", score: 2, img_link: imageList[0], location: "Location 3", activity: "Lecturing"  },
+    { date: "2022-01-02", score: 2, img_link: imageList[3], location: "Location 3", activity: "Drive to work"  },
+    { date: "2023-01-15", score: 3, img_link: imageList[2], location: "Location 4", activity: "Breakfast"  },
+    { date: "2023-12-15", score: 8, img_link: imageList[4], location: "Location 2", activity: "Drive to work"  },
+    { date: "2023-12-15", score: 4, img_link: imageList[7], location: "Location 4", activity: "Breakfast"  },
+    { date: "2023-01-02", score: 2, img_link: imageList[5], location: "Location 4", activity: "Lecturing"  },
+    { date: "2023-01-01", score: 5, img_link: imageList[5], location: "Location 1", activity: "Drive to work"  },
+    { date: "2023-01-01", score: 3, img_link: imageList[6], location: "Location 2", activity: "Drive to work"  },
+    { date: "2023-01-02", score: 2, img_link: imageList[8], location: "Location 1", activity: "Lecturing"  },
+    { date: "2023-01-02", score: 0, img_link: imageList[9], location: "Location 2", activity: "Lecturing"  },
+    { date: "2012-06-20", score: 3, img_link: imageList[3], location: "Location 1", activity: "Breakfast"  },
 ];
 
 const Home = ({selectedFilters}) => {
