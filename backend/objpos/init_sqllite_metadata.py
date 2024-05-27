f998a6252e0ecd26ac230ae450a7e9fd31b2c642
minhquanBr2 <minhquanhole03@gmail.com>
2024-05-27 16:13:41 +0700

    change sql

--- backend/objpos/init_sqllite_metadata.py
+++ backend/objpos/init_sqllite_metadata.py
@@ -6,12 +6,14 @@ conn = sqlite3.connect('metadata.db')
 cursor = conn.cursor()
 
 # Create a table to store image data
-cursor.execute('''CREATE TABLE IF NOT EXISTS metadata2 (
+cursor.execute('''CREATE TABLE IF NOT EXISTS metadata (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 filepath TEXT,
                 date TEXT,
                 time TEXT,
+                day_of_week TEXT,
                 location TEXT,
+                location_displayed TEXT,
                 location_id INTEGER,
                 new_lat REAL,
                 new_lng REAL,
@@ -19,18 +21,21 @@ cursor.execute('''CREATE TABLE IF NOT EXISTS metadata2 (
                 activity_id INTEGER,
                 event_id INTEGER,
                 caption TEXT,
-                ocr TEXT
+                ocr TEXT,
+                object_tags TEXT
                 )''')
 conn.commit()
 
 # Load metadata from CSV file
-metadata = pd.read_csv("metadata_response_5.csv")
+metadata = pd.read_csv("metadata_response_6.csv")
 metadata.set_index('image_link', inplace=True)
 count = 0
 for filepath in metadata.index:
     date = metadata.loc[filepath, 'date']
     time = metadata.loc[filepath, 'time']
+    day_of_week = metadata.loc[filepath, 'day_of_week']
     location = metadata.loc[filepath, 'location']
+    location_displayed = metadata.loc[filepath, 'location_displayed']
     location_id = metadata.loc[filepath, 'location_id']
     new_lat = metadata.loc[filepath, 'new_lat']
     new_lng = metadata.loc[filepath, 'new_lng']
@@ -39,10 +44,11 @@ for filepath in metadata.index:
     event_id = metadata.loc[filepath, 'event_id']
     caption = metadata.loc[filepath, 'caption']
     ocr = metadata.loc[filepath, 'ocr']
+    object_tags = metadata.loc[filepath, 'object_tags']
 
-    cursor.execute('''INSERT INTO metadata2 (filepath, date, time, location, location_id, new_lat, new_lng, activity, activity_id, event_id, caption, ocr)
-                        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)''',
-                    (filepath, date, time, location, int(location_id), new_lat, new_lng, activity, int(activity_id), int(event_id), caption, ocr))    
+    cursor.execute('''INSERT INTO metadata (filepath, date, time, day_of_week, location, location_displayed, location_id, new_lat, new_lng, activity, activity_id, event_id, caption, ocr, object_tags)
+                        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)''',
+                    (filepath, date, time, day_of_week, location, location_displayed, int(location_id), new_lat, new_lng, activity, int(activity_id), int(event_id), caption, ocr, object_tags))    
     count += 1
     if count % 1000 == 0:
         print(count)
