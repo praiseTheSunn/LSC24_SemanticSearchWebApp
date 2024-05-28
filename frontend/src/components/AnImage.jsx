d384c03031d72e91c182f85ec0419e2a0e783e10
EricNguyen1203 <21120132@student.hcmus.edu.vn>
2024-05-28 23:11:16 +0700

    merge vien

--- frontend/src/components/AnImage.jsx
+++ frontend/src/components/AnImage.jsx
@@ -1,3 +1,56 @@
+// import view_icon from '../assets/view_icon.png';
+// import React from 'react';
+// import { useNeighborPopup } from '../contexts/neighborPopupContext';
+// import { useSinglePopup } from '../contexts/singlePopupContext';
+// import { useState } from 'react';
+
+// const AnImage = ({ src, date, time, index, onClick }) => {
+//     const { openPopup } = useNeighborPopup();
+//     const { openSinglePopup } = useSinglePopup();
+//     const formattedTime = `${date}  ${time}`;
+
+//     // const [showNeighborPopup, setShowNeighborPopup] = useState(false);
+
+//     // const handleDoubleClick = () => {
+//     //     setShowNeighborPopup(true);
+//     // }
+
+//     return (
+//         <>
+//             {/* {showNeighborPopup && <NeighborPopup viewImage={src} />} */}
+//             <div key={index} className='img-container relative w-full h-full ' >
+//                 <style>
+//                     {`.img-container:hover .img-action { display: block;}`}
+//                 </style>
+//                 <div className='text-xs text-white bg-black opacity-60 absolute top-0 left-0 py-1'>{formattedTime}</div>
+//                 <img
+//                     src={src}
+//                     alt={`Image ${index}`}
+//                     className=' object-contain w-full max-h-[120px] cursor-pointer'
+//                     // onDoubleClick={handleDoubleClick}
+//                     onClick={() => openSinglePopup(src)}
+//                 />
+//                 <div className='bg-black opacity-50 absolute bottom-0 right-0 hidden img-action'
+//                 // onClick={}
+//                 >
+//                     <img
+//                         src={view_icon}
+//                         alt={`View ${index}`}
+//                         className='size-7'
+//                         onClick={() => openPopup(src)}
+//                     />
+//                 </div>
+
+//             </div>
+//         </>
+
+//     );
+// }
+
+// export default React.memo(AnImage);
+
+
+
 import view_icon from '../assets/view_icon.png';
 import React from 'react';
 import { useNeighborPopup } from '../contexts/neighborPopupContext';
--- frontend/src/components/Popup/singlePopup.jsx
+++ frontend/src/components/Popup/singlePopup.jsx
@@ -2,34 +2,26 @@ import './singlePopup.css'
 import closeIcon from '../../assets/close.png'
 import bcn from '../../assets/bcn.png'
 import ImageInList from '../Image/imageInList'
-import { useRef, useEffect, useState, useCallback  } from 'react'
+import { useRef, useEffect, useState, useCallback } from 'react'
 import imageService from '../../services/imageService'
 import { useSelectedImages } from '../../contexts/selectedImageContext'
 import { FixedSizeGrid as Grid } from 'react-window';
 import AutoSizer from 'react-virtualized-auto-sizer';
+import { AnImage } from '../../components';
 
-const SinglePopup = ({viewImage, onClose}) => {
-    const [neighborsData, setNeighborsData] = useState([]);
+const SinglePopup = ({ viewImage, onClose }) => {
+    const [singlePopupData, setsinglePopupData] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
-    const [hasScrolled, setHasScrolled] = useState(false);
     const viewImageRef = useRef(null);
     const gridRef = useRef(null);
-    const previousScrollTop = useRef(0);
 
-    const fetchNeighbors = useCallback(async (imageId) => {
+    const fetchSimilars = useCallback(async (imageId) => {
         setIsLoading(true);
         try {
             const response = await imageService.getSimilarImages2Image(imageId);
             const newNeighbors = response.data.response;
-            setNeighborsData(newNeighbors);
+            setsinglePopupData(newNeighbors);
             return newNeighbors;
-            // setNeighborsData(prev => {
-            //     if (position === 'start') {
-            //         return [...newNeighbors, ...prev];
-            //     } else {
-            //         return [...prev, ...newNeighbors];
-            //     }
-            // });
         } catch (error) {
             console.error('Error fetching Similar Images:', error);
         } finally {
@@ -39,52 +31,12 @@ const SinglePopup = ({viewImage, onClose}) => {
 
     useEffect(() => {
         const imageList = [viewImage];
-        fetchNeighbors(imageList, "stfm");
-    }, [viewImage, fetchNeighbors]);
-
-    useEffect(() => {
-        if (viewImageRef.current && !hasScrolled) {
-            viewImageRef.current.scrollIntoView({
-                behavior: 'smooth',
-                block: 'center',
-            });
-            setHasScrolled(true);
-        }
-    }, [neighborsData, viewImage, hasScrolled]);
-
-    const handleScroll = ({ scrollTop }) => {
-        const scrollDirection = scrollTop < previousScrollTop.current ? 'backward' : 'forward';
-        previousScrollTop.current = scrollTop;
-
-        if (scrollDirection === 'backward' && scrollTop === 0 && !isLoading) {
-            const firstImage = neighborsData[0]?.img_link;
-            const firstImageList = firstImage ? [firstImage] : [];
-            
-            console.log('firstImage', firstImage);
-            console.log('firstImageList', firstImageList);
-
-            if (firstImageList) {
-                fetchNeighbors(firstImageList, "stfm");
-            }
-        }
-
-        // if (scrollDirection === 'forward' && !isLoading) {
-        //     const grid = gridRef.current;
-        //     if (grid) {
-        //         const { scrollHeight, clientHeight } = grid._outerRef;
-        //         if (scrollTop + clientHeight >= scrollHeight) {
-        //             const lastImage = neighborsData[neighborsData.length - 1]?.img_link;
-        //             if (lastImage) {
-        //                 fetchNeighbors(lastImage, );
-        //             }
-        //         }
-        //     }
-        // }
-    };
+        fetchSimilars(imageList, "stfm");
+    }, [viewImage, fetchSimilars]);
 
     const Cell = ({ columnIndex, rowIndex, style }) => {
         const index = rowIndex * columnCount + columnIndex;
-        const data = neighborsData[index];
+        const data = singlePopupData[index];
         if (!data) return null;
 
         const { img_link, date, time } = data;
@@ -92,13 +44,18 @@ const SinglePopup = ({viewImage, onClose}) => {
         const isHighlighted = img_link === viewImage;
 
         return (
-            <div
-                className={`image-wrapper-neighbor ${isHighlighted ? 'highlight' : ''}`}
-                style={{ ...style, padding: '10px', boxSizing: 'border-box' }}
+            <div style={style} className={`max-h-[142px] image-wrapper-neighbor ${isHighlighted ? 'highlight' : ''}`}
                 ref={isHighlighted ? viewImageRef : null}
             >
-                <div className='overlay-neighbor'>{formattedTime}</div>
-                <img src={img_link} alt={`Image ${index}`} className='image-item-neighbor' />
+                <div className="h-auto image-item overflow-hidden" >
+                    <AnImage
+                        key={index}
+                        src={data.img_link}
+                        date={data.date}
+                        index={index}
+                        time={data.time}
+                    />
+                </div>
             </div>
         );
     };
@@ -107,37 +64,45 @@ const SinglePopup = ({viewImage, onClose}) => {
     const itemSize = 180; // Size of each cell in the grid
 
     return (
-        <div className='neighbor-popup-container'>
+        <div className='single-popup-container'>
             <div className='popup-content-background row'>
-                <div className='neighbor-image-container col h-full w-full'>
-                    <h1>Single</h1>
+                <div className='single-images-container col h-full w-full'>
+                    <h1>Similar Images</h1>
                     <br />
-                    {neighborsData.length > 0 ? (
-                        <AutoSizer>
-                            {({ height, width }) => {
-                                const columnWidth = width / columnCount;
-                                const rowHeight = 130; // Making rows square by setting row height equal to column width
-                                const rowCount = Math.ceil(neighborsData.length / columnCount);
+                    <div className='single-image-container'>
+                        <div className='left-column'>
+                            <img src={viewImage} alt='single-popup' className='centered-image'/>
+                        </div>
+                        <div className='right-column'>
+                            {singlePopupData.length > 0 ? (
+                                <AutoSizer>
+                                    {({ height, width }) => {
+                                        const columnWidth = width / columnCount;
+                                        const rowHeight = 130; // Making rows square by setting row height equal to column width
+                                        const rowCount = Math.ceil(singlePopupData.length / columnCount);
 
-                                return (
-                                    <Grid
-                                        columnCount={columnCount}
-                                        columnWidth={columnWidth}
-                                        height={height}
-                                        rowCount={rowCount}
-                                        rowHeight={rowHeight}
-                                        width={width}
-                                        // onScroll={({ scrollTop }) => handleScroll({ scrollTop })}
-                                        ref={gridRef}
-                                    >
-                                        {Cell}
-                                    </Grid>
-                                )
-                            }}
-                        </AutoSizer>
-                    ) : (
-                        <div>Loading neighbors...</div>
-                    )}
+                                        return (
+                                            <Grid
+                                                columnCount={columnCount}
+                                                columnWidth={columnWidth}
+                                                height={height}
+                                                rowCount={rowCount}
+                                                rowHeight={rowHeight}
+                                                width={width}
+                                                ref={gridRef}
+                                            >
+                                                {Cell}
+                                            </Grid>
+                                        )
+                                    }}
+                                </AutoSizer>
+                            ) : (
+                                <div>Loading Similar Images...</div>
+                            )}
+                        </div>
+
+                    </div>
+
                 </div>
 
                 <div className='close-button-container'>
