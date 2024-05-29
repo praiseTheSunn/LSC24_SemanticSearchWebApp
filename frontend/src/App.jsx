4b2631d8bf0dae55d88cb2d022f06a96558c698a
Hoang Do <hoang2003ngolike@gmail.com>
2024-05-29 19:32:09 +0700

    use context for popup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,19 @@
+import { Popup } from 'react-leaflet';
 import './App.css';
 import Routers from './Routers/Routers';
 import { EvaluationContext, EvaluationContextProvider } from './contexts/EvaluationContext';
 import { SelectedImagesProvider } from './contexts/selectedImageContext';
+import { PopUpProvider } from './contexts/popUpContext';
 
 function App() {
   return (
+    <PopUpProvider>
     <EvaluationContextProvider>
     <SelectedImagesProvider>
       <Routers />
     </SelectedImagesProvider>
     </EvaluationContextProvider>
+    </PopUpProvider>
   );
 }
 
