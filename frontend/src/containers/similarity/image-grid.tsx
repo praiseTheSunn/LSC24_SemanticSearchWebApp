2acd1e6ce716040e4655c4c472f1c9a8227efbaa
hdkhang6803 <hdkhang6803@gmail.com>
2024-08-16 16:52:15 +0700

    trivial

--- frontend/src/containers/similarity/image-grid.tsx
+++ frontend/src/containers/similarity/image-grid.tsx
@@ -9,6 +9,8 @@ const ImageGrid = ({ cellHeight, cell } : {
 }) => {
   cellHeight = cellHeight ? cellHeight : 125 // Default cell height
 
+  const simData = useAppSelector((state) => state.app.data);
+
   const columnCount = 9 // Number of columns in the grid
 
   const Cell = ({ columnIndex, rowIndex, style } : {
