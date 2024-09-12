4ed3b4317a60f2a540d6e3445cb90b316fbefa52
Hoang Do <hoang2003ngolike@gmail.com>
2024-09-13 03:57:38 +0700

    implement Popover for handling popup window

--- frontend/src/components/CSVDownloadBox.tsx
+++ frontend/src/components/CSVDownloadBox.tsx
@@ -1,33 +1,32 @@
-import { Box, Typography, SpeedDial, SpeedDialAction, ClickAwayListener } from "@mui/material";
+import { Box, SpeedDial, SpeedDialAction, ClickAwayListener, Popover, SpeedDialIcon, Snackbar } from "@mui/material";
 import LoginIcon from '@mui/icons-material/Login';
 import FileDownloadIcon from "@mui/icons-material/FileDownload";
-import CalendarViewMonthIcon from '@mui/icons-material/CalendarViewMonth';
 import ClearIcon from "@mui/icons-material/Clear";
 import PreviewIcon from "@mui/icons-material/Preview";
 import { appActions, useAppDispatch, useAppSelector } from "../AppState";
 import { toast } from "react-toastify";
 import { CSVPreviewPopup } from "./Popup/CSVPreviewPopup";
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
 import EvaluationBox from "./evaluationBox";
 import MenuIcon from '@mui/icons-material/Menu';
-import CloseIcon from '@mui/icons-material/Close';
 
 export const CSVDownloadBox = () => {
     const csvImages = useAppSelector((state) => state.app.csvImages);
     const dispatch = useAppDispatch();
 
-    const CSVPreviewPopupOpen = useAppSelector((state) => state.app.isCsvPreviewPopupOpen);
-    const isVisible = useAppSelector((state) => state.app.isEvaluationBoxOpen);
+    const [anchorElCSV, setAnchorElCSV] = useState<HTMLElement | null>(null);
+    const [anchorElEvaluation, setAnchorElEvaluation] = useState<HTMLElement | null>(null);
+    const [speedDialOpen, setSpeedDialOpen] = useState(false); // New state for SpeedDial open
+    const CSVPreviewPopupOpen = Boolean(anchorElCSV);
+    const isVisible = Boolean(anchorElEvaluation);
 
-    const toggleCSVPreviewPopup = useCallback(() => {
-        const value = !CSVPreviewPopupOpen;
-        dispatch(appActions.setCSVPreviewPopup(value))
-      }, [CSVPreviewPopupOpen, dispatch])
+    const toggleCSVPreviewPopup = useCallback((event: React.MouseEvent<HTMLElement>) => {
+        setAnchorElCSV(CSVPreviewPopupOpen ? null : event.currentTarget);
+    }, [CSVPreviewPopupOpen]);
 
-    const toggleEvaluationBox = useCallback(() => {
-        const value = !isVisible;
-        dispatch(appActions.setEvaluationBox(value));
-    }, [isVisible, dispatch]);
+    const toggleEvaluationBox = useCallback((event: React.MouseEvent<HTMLElement>) => {
+        setAnchorElEvaluation(isVisible ? null : event.currentTarget);
+    }, [isVisible]);
 
     const handleDownloadCSV = () => {
         if (csvImages.length > 0) {
@@ -40,83 +39,119 @@ export const CSVDownloadBox = () => {
             hiddenElement.download = 'images.csv';
             hiddenElement.click();
         } else {
-            toast.error('No images to download');
+            toast.error('No images to download', {
+                position: "bottom-left"
+              });
+              
+            
         }
     };
 
     const handleClearCSV = () => {
         dispatch(appActions.setCSVImages([]));
-        toast.success('Cleared');
+        toast.success('Cleared', {
+            position: "bottom-left"
+        });
     };
 
-    const handlePreviewCSV = () => {
-        toggleCSVPreviewPopup();
+    const handlePreviewCSV = (event: React.MouseEvent<HTMLElement>) => {
+        toggleCSVPreviewPopup(event);
     };
 
-    const handleLogin = () => {
-        toggleEvaluationBox();
+    const handlePreviewCSVOpen = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorElCSV(event.currentTarget);
+    }
+
+    const handlePreviewCSVClose = () => {
+        console.log('close')
+        setAnchorElCSV(null);
+    }
+
+    const handleLogin = (event: React.MouseEvent<HTMLElement>) => {
+        toggleEvaluationBox(event);
+    };
+
+    const handleLoginOpen = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorElEvaluation(event.currentTarget);
+    }
+
+    const handleLoginClose = () => {
+        setAnchorElEvaluation(null);
     }
 
     return (
         <ClickAwayListener onClickAway={() => {
-            dispatch(appActions.setCSVPreviewPopup(false))
-            dispatch(appActions.setEvaluationBox(false))
+            // setAnchorElCSV(null);
+            // setAnchorElEvaluation(null);
+            setSpeedDialOpen(false)
         }}>
-        <Box sx={{ 
-            position: 'relative',
-            // display: 'flex', 
-            // flexDirection: 'column', 
-            alignItems: 'center', 
-            // padding: '9px', 
-            zIndex: '9999', 
-            width: '50px'
+            <Box sx={{ 
+                position: 'relative',
+                alignItems: 'center', 
+                zIndex: '9999', 
+                width: '50px'
             }}>
-            {/* <Typography
-                variant="caption"
-                sx={{ display: 'flex', fontSize: '11px', color: 'gray', opacity: '0.7', fontStyle: 'italic' }}
-            >
-                Shift click an image to add to csv lists
-            </Typography> */}
-            <SpeedDial
-                ariaLabel="CSV actions"
-                sx={{ 
-                    position: 'absolute',
-                    width: '100%',
-                    marginTop: '-20px',
-                }}
-                icon={<MenuIcon/>} // This is the default SpeedDial icon, replace as needed
-                direction="down"
-            >
-                <SpeedDialAction
-                    icon={<FileDownloadIcon />}
-                    tooltipTitle="Download CSV"
-                    onClick={handleDownloadCSV}
-                />
-                <SpeedDialAction icon={<ClearIcon />} tooltipTitle="Clear CSV" onClick={handleClearCSV} />
-                <SpeedDialAction icon={<PreviewIcon />} tooltipTitle="Preview CSV" onClick={handlePreviewCSV} />
-                <SpeedDialAction icon={<LoginIcon />} tooltipTitle="Login" onClick={handleLogin} />
-            </SpeedDial>
+                <SpeedDial
+                    ariaLabel="CSV actions"
+                    sx={{ 
+                        position: 'absolute',
+                        width: '100%',
+                        marginTop: '-20px',
+                    }}
+                    // icon={<MenuIcon />}
+                    icon={<SpeedDialIcon />}
+                    direction="down"
+                    open={speedDialOpen}
+                    // onOpen={() => setSpeedDialOpen(true)}
+                    onMouseEnter={() => setSpeedDialOpen(true)}
+                    onMouseLeave={() => {
+                        // setSpeedDialOpen(false)
+                        // handlePreviewCSVClose()
+                    }}
+                    // onClick={() => setSpeedDialOpen(!speedDialOpen)}
+                >
+                    <SpeedDialAction
+                        icon={<FileDownloadIcon />}
+                        tooltipTitle="Download CSV"
+                        onClick={handleDownloadCSV}
+                    />
+                    <SpeedDialAction icon={<ClearIcon />} tooltipTitle="Clear CSV" onClick={handleClearCSV} />
+                    <SpeedDialAction icon={<PreviewIcon />} tooltipTitle="Preview CSV" onMouseEnter={handlePreviewCSVOpen} />
+                    <SpeedDialAction icon={<LoginIcon />} tooltipTitle="Login" onMouseEnter={handleLoginOpen} />
+                </SpeedDial>
 
-            <Box
-            sx = {{
-                position: 'absolute',
-                right: '80px',
-            }}
-            >
-            {CSVPreviewPopupOpen && <CSVPreviewPopup />}
-            </Box>
+                <Popover
+                    open={CSVPreviewPopupOpen}
+                    anchorEl={anchorElCSV}
+                    onClose={handlePreviewCSVClose}
+                    anchorOrigin={{
+                        vertical: 'center',
+                        horizontal: 'left',
+                    }}
+                    transformOrigin={{
+                        vertical: 'center',
+                        horizontal: 'right',
+                    }}
+                >
+                    <CSVPreviewPopup />
+                </Popover>
 
-            <Box
-            sx = {{
-                position: 'absolute',
-                right: '100px',  
-            }}
-            >
-            {isVisible && <EvaluationBox />}
+                <Popover
+                    open={isVisible}
+                    anchorEl={anchorElEvaluation}
+                    onClose={handleLoginClose}
+                    anchorOrigin={{
+                        vertical: 'center',
+                        horizontal: 'left',
+                    }}
+                    transformOrigin={{
+                        vertical: 'center',
+                        horizontal: 'right',
+                    }}
+                >
+                    <EvaluationBox />
+                </Popover>
             </Box>
-            
-            
-        </Box>
         </ClickAwayListener>
     );
 };
