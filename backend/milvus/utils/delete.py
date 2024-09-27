8433b4920b49fc231fb3006804e21a1780d519ae
minhquanBr2 <minhquanhole03@gmail.com>
2024-09-27 18:06:56 +0000

    add blip2 ebd

--- backend/milvus/utils/delete.py
+++ backend/milvus/utils/delete.py
@@ -14,7 +14,7 @@ TOKEN = "root:Milvus"
 
 # 1. Set up a Milvus client
 client = MilvusClient(uri=CLUSTER_ENDPOINT, token=TOKEN)
-client.drop_collection(collection_name='aic24_clip')
+client.drop_collection(collection_name='aic24_blip2')
 
 
 collections = utility.list_collections()
