6fe771e295a3e0ab6dd87b6aef0b9a2be166eeb2
minhquanBr2 <minhquanhole03@gmail.com>
2024-10-05 21:29:20 +0700

    modify server

--- frontend/src/types/constants.ts
+++ frontend/src/types/constants.ts
@@ -1,3 +1,5 @@
-export const BASE_API_URL = 'http://34.87.27.174:8001/'
-export const OBJ_POS_API_URL = 'http://34.87.27.174:8001/'
+export const BASE_API_URL = 'http://35.185.176.85:8001/'
+export const OBJ_POS_API_URL = 'http://35.185.176.85:8001/'
+// export const BASE_API_URL = 'http://178.128.117.254:8001/'
+// export const OBJ_POS_API_URL = 'http://178.128.117.254:8001/'
 export const EVAL_API_URL = 'http://'
