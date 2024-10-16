bc93140b8700c2fe196ff540d38c60ea37d1815b
hdkhang6803 <hdkhang6803@gmail.com>
2024-10-16 11:07:37 +0700

    minor changes in transform ip in image link

--- frontend/src/config/transformResponse.ts
+++ frontend/src/config/transformResponse.ts
@@ -7,7 +7,7 @@ export const transformResponse_AIC2024 = (response: ApiResponse) => {
   const result = data.map((img: ImageRecord) => {
     img.date = img.video_id ? img.video_id : img.date
     img.time = img.frame_id ? img.frame_id : img.time
-    img.img_link = img.img_link.replace('178.128.117.254/AIC_IMAGE', '127.0.0.1:8080')
+    img.img_link = img.img_link.replace('178.128.117.254', '127.0.0.1:8080')
     return img
   })
   return result
