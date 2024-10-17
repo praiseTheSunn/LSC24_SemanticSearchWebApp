3e293eced5d6fca10bd719a9714f35f925907d88
minhquanBr2 <minhquanhole03@gmail.com>
2024-10-17 09:03:10 +0000

    done temporal for keywords

--- backend/main/internal/search/search.py
+++ backend/main/internal/search/search.py
@@ -21,7 +21,7 @@ def search_with_image_query(data: RequestSearchByImageQuery):
         if model == "stfm":                                             # image embedding must be at format [[]], but 'stfm' model returns [] so I have to wrap it
             image_embedding = [image_embedding]
         
-        results_semantic = search_semantic(dataset, model, [image_embedding])  
+        results_semantic = search_semantic_temporal(dataset, model, [image_embedding])  
         return prepare_response(results_semantic["urls"], results_semantic["scores"]), status.HTTP_200_OK            
     else:
         return response.text, response.status_code
@@ -63,25 +63,25 @@ def search_with_text_query(data: RequestSearchByTextQuery):
     
     # Mode: semantic, objects
     if mode == "vec":
-        results_semantic = search_semantic(dataset, model, text_embeddings) if text_query else None
+        results_semantic = search_semantic_temporal(dataset, model, text_embeddings) if text_query else None
         urls_semantic = results_semantic["urls"] if results_semantic else []
         results_objects = search_objects(dataset, object_local_encoding, color_local_encoding, pose_local_encoding, subset=urls_semantic) if (object_local_encoding or color_local_encoding) else None   
         combined = combine_score.get_combined_scores([results_semantic, results_objects], 'inner')
         return prepare_response(combined["urls"], combined["scores"]), status.HTTP_200_OK
     
-    # Mode: semantic, keyword, objects
+    # Mode: semantic, keywords, objects
     if mode == "vec_kw":
-        results_semantic = search_semantic(dataset, model, text_embeddings) if text_query else None 
+        results_semantic = search_semantic_temporal(dataset, model, text_embeddings) if text_query else None 
         urls_semantic = results_semantic["urls"] if results_semantic else []
-        results_keywords = search_keyword(dataset, text_query, subset=urls_semantic) if text_query else None
+        results_keywords = search_keywords_temporal(dataset, text_query, subset=urls_semantic) if text_query else None
         urls_keywords = results_keywords["urls"] if results_keywords else []
         results_objects = search_objects(dataset, object_local_encoding, color_local_encoding, pose_local_encoding, subset=urls_keywords) if (object_local_encoding or color_local_encoding) else None   
         combined = combine_score.get_combined_scores([results_semantic, results_keywords, results_objects], 'inner')
         return prepare_response(combined["urls"], combined["scores"]), status.HTTP_200_OK
 
-    # Mode: keyword, objects
+    # Mode: keywords, objects
     if mode == "kw":
-        results_keywords = search_keyword(dataset, text_query) if text_query else None
+        results_keywords = search_keywords_temporal(dataset, text_query) if text_query else None
         urls_keywords = results_keywords["urls"] if results_keywords else []
         results_objects = search_objects(dataset, object_local_encoding, color_local_encoding, pose_local_encoding, subset=urls_keywords) if (object_local_encoding or color_local_encoding) else None   
         combined = combine_score.get_combined_scores([results_keywords, results_objects], 'inner')
@@ -91,21 +91,21 @@ def search_with_text_query(data: RequestSearchByTextQuery):
         
         # # Mode: semantic x datetime
         # if mode == "smt-dtout":
-        #     results_semantic = search_semantic(model, text_embedding)   
+        #     results_semantic = search_semantic_temporal(model, text_embedding)   
         #     results_datetime = search_datetime(text_query)
         #     combined = combine_score.get_combined_scores_datetime([results_semantic], results_datetime)
         #     return prepare_response(combined["urls"], combined["scores"]), status.HTTP_200_OK
 
         # Mode: semantic + multimatch (datetime included)
         if mode == "smt-mm-dtin":
-            results_semantic = search_semantic(model, text_embedding)
-            results_keywords = search_keyword(text_query)
+            results_semantic = search_semantic_temporal(model, text_embedding)
+            results_keywords = search_keywords_temporal(text_query)
             combined = combine_score.get_combined_scores([results_semantic, results_keywords])
             return prepare_response(combined["urls"], combined["scores"]), status.HTTP_200_OK
         
         # # Mode: (semantic + multimatch) x datetime
         # if mode == "smt-mm-dtout":
-        #     results_semantic = search_semantic(model, text_embedding)
+        #     results_semantic = search_semantic_temporal(model, text_embedding)
         #     results_multimatch = search_multimatch(text_query)
         #     results_datetime = search_datetime(text_query)
         #     combined = combine_score.get_combined_scores_datetime([results_semantic, results_multimatch], results_datetime)
@@ -113,14 +113,14 @@ def search_with_text_query(data: RequestSearchByTextQuery):
         
         # # Mode: semantic + 3 matches (datetime included)
         # if mode == "smt-3m-dtin":
-        #     results_semantic = search_semantic(model, text_embedding)
+        #     results_semantic = search_semantic_temporal(model, text_embedding)
         #     results_3match_datetime = search_3match_datetime(text_query)
         #     combined = combine_score.get_combined_scores([results_semantic, results_3match_datetime])
         #     return prepare_response(combined["urls"], combined["scores"]), status.HTTP_200_OK
         
         # # Mode: (semantic + 3 matches) x datetime
         # if mode == "smt-3m-dtout":
-        #     results_semantic = search_semantic(model, text_embedding)
+        #     results_semantic = search_semantic_temporal(model, text_embedding)
         #     results_objects_tags = search_match_object_tags(text_query)
         #     results_place = search_match_location(text_query)
         #     results_caption = search_match_caption(text_query)
