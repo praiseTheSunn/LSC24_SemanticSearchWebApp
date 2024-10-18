10aab1a279ad261269c6e9b67f60a599334610ab
unknown <vohoanghoavien@gmail.com>
2024-10-18 12:27:39 +0700

    Add Response

--- frontend/src/components/evaluationBox.tsx
+++ frontend/src/components/evaluationBox.tsx
@@ -1,98 +1,3 @@
-// import React, { useEffect, useState, useContext } from 'react'
-// import { toast } from 'react-toastify'
-// // import evalService from '../services/evalService'
-// import { appActions, evaluationActions, useAppDispatch } from '../AppState'
-// import { useCallback } from 'react'
-// import { useSelector } from 'react-redux'
-// import { isNil } from 'lodash'
-// import type { EvaluationState } from '../types/app'
-
-// const EvaluationBox = () => {
-
-//   const [text, setText] = useState('')
-
-//   const [loginState, setLoginState] = useState('Login')
-
-//   const dispatch = useAppDispatch()
-//   const isEvaluationIdNull = useSelector((state: EvaluationState) => state.evaluationId, isNil)
-
-//   const evaluationId = useSelector((state: EvaluationState) => state.evaluationId)
-//   const username = useSelector((state: EvaluationState) => state.username)
-//   const password = useSelector((state: EvaluationState) => state.password)
-
-//   const setEvaluationId = useCallback((evaluationId : string) => {
-//     dispatch(evaluationActions.setEvaluationId(evaluationId))
-//   }, [dispatch])
-//   const setUsername = useCallback((username: string) => {
-//     dispatch(evaluationActions.setUsername(username))
-//   }, [dispatch])
-//   const setPassword = useCallback((password: string) => {
-//     dispatch(evaluationActions.setPassword(password))
-//   }, [dispatch])
-
-//   useEffect(() => {
-//     const session = localStorage.getItem('session')
-//     const username = localStorage.getItem('username')
-//     const password = localStorage.getItem('password')
-//     if (session && username && password) {
-//       setLoginState('Logout')
-//     }
-//     if (username) {
-//       setUsername(username)
-//     }
-//     if (password) {
-//       setPassword(password)
-//     }
-//   }, [setPassword, setUsername])
-
-//   return (
-//     <div style={{ display: 'grid', gridTemplateColumns: 'repeat(3, 1fr)', gap: '8px', width: 'auto', height: 'auto', backgroundColor: 'white', padding: '8px', borderRadius: '8px' }}>
-//       <input
-//         type="text"
-//         placeholder="Username"
-//         style={{ gridColumn: 'span 1', borderRadius: '8px', paddingLeft: '8px', backgroundColor: '#CBD5E1' }}
-//         value={username}
-//         onChange={(e) => setUsername(e.target.value)}
-//       />
-//       <input
-//         type="password"
-//         placeholder="Password"
-//         style={{ gridColumn: 'span 1', borderRadius: '8px', paddingLeft: '8px', backgroundColor: '#CBD5E1' }}
-//         value={password}
-//         onChange={(e) => setPassword(e.target.value)}
-//       />
-//       <button
-//         type="button"
-//         style={{ gridColumn: 'span 1', borderRadius: '8px', paddingLeft: '8px', backgroundColor: '#64748B', color: '#FFFFFF' }}
-//       >
-//         {loginState}
-//       </button>
-//       <input
-//         type="text"
-//         placeholder="Evaluation ID"
-//         style={{ gridColumn: 'span 3', borderRadius: '8px', paddingLeft: '8px', zIndex: 100, backgroundColor: '#CBD5E1' }}
-//         value={evaluationId ?? ''}
-//         onChange={(e) => setEvaluationId(e.target.value)}
-//       />
-//       <input
-//         type="text"
-//         placeholder="Text"
-//         style={{ gridColumn: 'span 2', borderRadius: '8px', paddingLeft: '8px', backgroundColor: '#CBD5E1' }}
-//         value={text}
-//         onChange={(e) => setText(e.target.value)}
-//       />
-//       <button
-//         type="button"
-//         style={{ gridColumn: 'span 1', borderRadius: '8px', paddingLeft: '8px', backgroundColor: '#64748B', color: '#FFFFFF' }}
-//       >
-//         Submit text
-//       </button>
-//     </div>
-//   )
-// }
-
-// export default EvaluationBox
-
 import { Box, Button, Paper, TextField } from '@mui/material'
 import { isNil, set } from 'lodash'
 import React, { useEffect, useState, useCallback } from 'react'
@@ -109,7 +14,6 @@ const EvaluationBox = () => {
   const [trigger, result] = useLazyGetSessionIDQuery()
   const [triggerEval, resultEval] = useLazyGetEvalIDQuery()
   const [triggerQA, resultQA] = useQuestionAnsweringMutation()
-  // const [triggerKIS, resultKIS] = useKISAnsweringMutation()
   const dispatch = useAppDispatch()
 
   const username = useSelector((state: any) => state.evaluation.username)
@@ -169,21 +73,43 @@ const EvaluationBox = () => {
   useEffect(() => {
     if (resultEval.data) {
       console.log("evaluation here")
-      setEvaluationId(resultEval.data[0])
+      // setEvaluationId(resultEval.data[0])
+      setEvaluationId(resultEval.data[2])
+
     }
   }, [resultEval.data])
 
   useEffect(() => {
-    if (resultQA.data) {
-      console.log(resultQA.data.submission)
+    console.log('QA result:', resultQA); // In ra response khi có dữ liệu
+    if (resultQA && resultQA.data) {
+      console.log('QA result:', resultQA.data); // In ra response khi có dữ liệu
+      if (resultQA.data.status === true && resultQA.data.submission == "CORRECT") {
+        toast.success('Submission CORRECT', {
+          position: 'bottom-right',
+          autoClose: 2000,
+        })
+      }
+      else if (resultQA.data.status === true && resultQA.data.submission == "WRONG") {
+        toast.error('Submission WRONG', {
+          position: 'bottom-right',
+          autoClose: 2000,
+        })
+      }
+      else {
+        toast.error(`Submission FAILED ${resultQA.data.description}`, {
+          position: 'bottom-right',
+          autoClose: 2000,
+        })
+      }
     }
-  }, [resultQA.data])
-
-  // useEffect(() => {
-  //   if (resultKIS.data) {
-  //     console.log(resultKIS.data.submission)
-  //   }
-  // }, [resultKIS.isFetching])
+    if (resultQA && resultQA.isError) {
+      // console.error('Error from KIS:', resultKIS.error); // In ra lỗi nếu có
+      toast.error(`Submission FAILED - ERROR ${resultQA.error.data.description}`, {
+        position: 'bottom-right',
+        autoClose: 2000,
+      })
+    }
+  }, [resultQA]);
 
   const GetSessionID = () => {
     if (loginState === 'Login') {
@@ -196,61 +122,76 @@ const EvaluationBox = () => {
   }
 
   const SubmitText = () => {
-    if(resultEval.data && result.data){
-      triggerQA({ evaluation_id: resultEval.data[0], session: result.data, text: text })
+    if (resultEval.data && result.data) {
+      // Kiểm tra text có dạng "answer-Lxx_Vxxx-ms" không với answer khác chuỗi rỗng, x có dạng số, ms có dạng số
+      const regex = /^[^\s]+-L\d{2}_V\d{3}-\d+$/
 
-      toast.success(`Submitted with awser ${text}`, {
-        position: 'bottom-right',
-        autoClose: 5000,
-        closeOnClick: true,
-      })
-      // triggerKIS({ evaluation_id: resultEval.data, session: result.data, mediaItemName: "L03_V006", start: 891500, end: 891500 })
+      if (!regex.test(text)) {
+        toast.error('Text is not in the correct format, must be answer-Lxx_Vxxx-ms', {
+          position: 'bottom-right',
+          autoClose: 2000,
+          closeOnClick: true,
+        })
+      }
+      else {
+        // triggerQA({ evaluation_id: resultEval.data[0], session: result.data, text: text })
+        triggerQA({ evaluation_id: resultEval.data[2], session: result.data, text: text })
+
+        toast.success(`Submitted with awser ${text}`, {
+          position: 'bottom-right',
+          autoClose: 2000,
+          closeOnClick: true,
+        })
+        // triggerKIS({ evaluation_id: resultEval.data, session: result.data, mediaItemName: "L03_V006", start: 891500, end: 891500 })  
+      }
     }
   }
 
-  return (
-    <Paper
-      elevation={4}
-      sx={{
-        // width: '100%',
-        width: '500px',
-        position: 'relative',
-        display: 'grid',
-        gridTemplateColumns: 'repeat(3, 1fr)',
-        gap: 2,
-        p: 2,
-        backgroundColor: 'white',
-        borderRadius: 2,
-      }}
-    // style={{ position: 'absolute', top: '90px', right: '0px' }}
+
+
+return (
+  <Paper
+    elevation={4}
+    sx={{
+      // width: '100%',
+      width: '500px',
+      position: 'relative',
+      display: 'grid',
+      gridTemplateColumns: 'repeat(3, 1fr)',
+      gap: 2,
+      p: 2,
+      backgroundColor: 'white',
+      borderRadius: 2,
+    }}
+  // style={{ position: 'absolute', top: '90px', right: '0px' }}
+  >
+    <TextField
+      label="Username"
+      variant="outlined"
+      value={username}
+      onChange={(e) => setUsername(e.target.value)}
+      sx={{ gridColumn: 'span 1' }}
+      size="small"
+    />
+    <TextField
+      label="Password"
+      type="password"
+      variant="outlined"
+      value={password}
+      onChange={(e) => setPassword(e.target.value)}
+      sx={{ gridColumn: 'span 1' }}
+      size="small"
+    />
+    <Button
+      variant="contained"
+      color="primary"
+      sx={{ gridColumn: 'span 1' }}
+      size="small"
+      onClick={GetSessionID}
     >
-      <TextField
-        label="Username"
-        variant="outlined"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-        sx={{ gridColumn: 'span 1' }}
-        size="small"
-      />
-      <TextField
-        label="Password"
-        type="password"
-        variant="outlined"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        sx={{ gridColumn: 'span 1' }}
-        size="small"
-      />
-      <Button
-        variant="contained"
-        color="primary"
-        sx={{ gridColumn: 'span 1' }}
-        size="small"
-        onClick={GetSessionID}
-      >
-        {loginState}
-      </Button>
-      {/* <TextField
+      {loginState}
+    </Button>
+    {/* <TextField
         label="Evaluation ID"
         variant="outlined"
         value={evaluationId ?? ''}
@@ -258,25 +199,25 @@ const EvaluationBox = () => {
         sx={{ gridColumn: 'span 3' }}
         size="small"
       /> */}
-      <TextField
-        label="Text"
-        variant="outlined"
-        value={text}
-        onChange={(e) => setText(e.target.value)}
-        sx={{ gridColumn: 'span 2' }}
-        size="small"
-      />
-      <Button
-        variant="contained"
-        color="primary"
-        sx={{ gridColumn: 'span 1' }}
-        size="small"
-        onClick={SubmitText}
-      >
-        Submit Text
-      </Button>
-    </Paper>
-  )
+    <TextField
+      label="Text"
+      variant="outlined"
+      value={text}
+      onChange={(e) => setText(e.target.value)}
+      sx={{ gridColumn: 'span 2' }}
+      size="small"
+    />
+    <Button
+      variant="contained"
+      color="primary"
+      sx={{ gridColumn: 'span 1' }}
+      size="small"
+      onClick={SubmitText}
+    >
+      Submit Text
+    </Button>
+  </Paper>
+)
 }
 
 export default EvaluationBox
