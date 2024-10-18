4e4ebe72591e7bf9c3fa525af8f7fde0618c17c2
unknown <vohoanghoavien@gmail.com>
2024-10-18 21:46:26 +0700

    change feedback

--- frontend/src/components/CSVDownloadBox.tsx
+++ frontend/src/components/CSVDownloadBox.tsx
@@ -124,6 +124,7 @@ export const CSVDownloadBox = () => {
       feedbackData.like = {
         text_query: queryPayload.text_query,
         image_urls: likeImages.map((image) => image.img_link),
+        prior_scores: likeImages.map((image) => image.score),
         limit: likeLimit,
       };
       feedbackData.dislike = {
