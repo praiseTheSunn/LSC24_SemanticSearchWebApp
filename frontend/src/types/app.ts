4115ada9ae3c2c960eea42d1b24a598778dfae04
Hoang Do <hoang2003ngolike@gmail.com>
2024-10-19 10:18:10 +0700

    fix format

--- frontend/src/types/app.ts
+++ frontend/src/types/app.ts
@@ -66,7 +66,7 @@ export type ConfigType = {
 
   NeighborPopupCellHeight: number
   NeighborPopupColumnCount: number
-  NeighborPopupSpan: number 
+  NeighborPopupSpan: number
 
   WhiteboardGridRowCount: number
   WhiteboardGridColumnCount: number
