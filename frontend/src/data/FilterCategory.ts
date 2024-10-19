e24c7bb16eb9a4a5cc3d4d6811f470cf936aadde
hdkhang6803 <hdkhang6803@gmail.com>
2024-10-19 14:42:26 +0700

    add context filter

--- frontend/src/data/FilterCategory.ts
+++ frontend/src/data/FilterCategory.ts
@@ -4,7 +4,8 @@ const FilterCategories_AIC2024 = {
   '-d': { category: 'date', startIndex: 3, display: '-d ... : date' },
   '-ocr': { category: 'ocr', startIndex: 5, display: '-ocr ... : OCR text' },
   '-obj': { category: 'object_tags', startIndex: 5, display: '-obj ... : Object Detection' },
-  '-c': { category: 'caption', startIndex: 3, display: '-c ... : Caption' },
+  '-cap': { category: 'caption', startIndex: 5, display: '-cap ... : Caption' },
+  '-ctx': { category: 'context_en_keywords', startIndex: 5, display: '-ctx ... : Context' },
   '-v': { category: 'video_id', startIndex: 3, display: '-v ... : Video ID' },
 }
 
