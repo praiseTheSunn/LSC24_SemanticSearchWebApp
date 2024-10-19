10c6357deb4ff5c8f411d4edbbdd24e4aa6ce8c3
hdkhang6803 <hdkhang6803@gmail.com>
2024-10-19 14:31:08 +0700

    refactor: fuse.js filter

---
+++ frontend/src/data/FilterCategory.ts
@@ -0,0 +1,12 @@
+const FilterCategories_AIC2024 = {
+  '-lo': { category: 'location', startIndex: 4, display: '-lo ... : location' },
+  '-t': { category: 'timestamp', startIndex: 3, display: '-t ... : time' },
+  '-d': { category: 'date', startIndex: 3, display: '-d ... : date' },
+  '-ocr': { category: 'ocr', startIndex: 5, display: '-ocr ... : OCR text' },
+  '-obj': { category: 'object_tags', startIndex: 5, display: '-obj ... : Object Detection' },
+  '-c': { category: 'caption', startIndex: 3, display: '-c ... : Caption' },
+  '-v': { category: 'video_id', startIndex: 3, display: '-v ... : Video ID' },
+}
+
+
+export const FilterCategories = FilterCategories_AIC2024
