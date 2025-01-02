e2ad613e5f9ceb9eb364f8bb1850cb1c7261f26e
minhquanBr2 <minhquanhole03@gmail.com>
2025-01-02 02:26:59 +0000

    rerun backend for vbs

--- backend/milvus/run.sh
+++ backend/milvus/run.sh
@@ -3,5 +3,5 @@ conda activate milvus
 conda env export > environment.yml
 sudo docker compose up -d
 
-export DATASET_CONFIG=../configs/aic24_config.yaml
-python3 -m uvicorn main:app --host 0.0.0.0 --port 8004 --reload
+export DATASET_CONFIG=../configs/vbs25_config.yaml
+python3 -m uvicorn main:app --host 0.0.0.0 --port 8003 --reload
