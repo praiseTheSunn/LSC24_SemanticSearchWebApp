a0750041561a5815e85961a33ec85b40d39b093f
minhquanBr2 <minhquanhole03@gmail.com>
2025-01-02 02:26:10 +0000

    rerun backend

--- backend/embedding_compute/setup.py
+++ backend/embedding_compute/setup.py
@@ -31,15 +31,15 @@ print(f"Done loading clip model in {time.time() - start_time} seconds.\n")
 # clip_v32_model, clip_v32_preprocess = clip.load("ViT-B/32")
 # print(f"Done loading clip_v32 model in {time.time() - start_time} seconds.\n")
 
-# blip2
-from torch import hub
-from lavis.models import load_model_and_preprocess
-print("Loading blip2 model...")
-start_time = time.time()
-# hub.set_dir(system_config['models']['blip2_model_path'])
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-blip2_model, blip2_vis_processors, blip2_txt_processors = load_model_and_preprocess(name="blip2_feature_extractor", model_type="pretrain", is_eval=True, device=device)
-print(f"Done loading blip2 model in {time.time() - start_time} seconds.\n")
+# # blip2
+# from torch import hub
+# from lavis.models import load_model_and_preprocess
+# print("Loading blip2 model...")
+# start_time = time.time()
+# # hub.set_dir(system_config['models']['blip2_model_path'])
+# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+# blip2_model, blip2_vis_processors, blip2_txt_processors = load_model_and_preprocess(name="blip2_feature_extractor", model_type="pretrain", is_eval=True, device=device)
+# print(f"Done loading blip2 model in {time.time() - start_time} seconds.\n")
 
 # # sentence_transformers
 # print("Loading stfm model...")
