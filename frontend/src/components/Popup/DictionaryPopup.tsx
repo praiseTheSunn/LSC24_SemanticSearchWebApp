fbaa6c49a6355f1810e354af7df2eb8e648ef28b
EricNguyen1203 <21120132@student.hcmus.edu.vn>
2025-01-03 23:08:42 +0700

    feat: add MVK and LHE Dictionary

--- frontend/src/components/Popup/DictionaryPopup.tsx
+++ frontend/src/components/Popup/DictionaryPopup.tsx
@@ -1,31 +1,40 @@
 import { Box } from '@mui/material'
 import { MVKImages } from '../../data/MVKImages'
+import { LHEImages } from '../../data/LHEImages'
 import { FixedSizeGrid as Grid } from 'react-window'
 import AutoSizer from 'react-virtualized-auto-sizer'
 import { pad } from 'lodash'
 import { Margin } from '@mui/icons-material'
 import bodyPartToIndex from '../../data/JointMapping'
+import { useState } from 'react'
+import { TrapoziedBgGrayLeft, TrapoziedBgGray2 } from '../../assets'
+import { List } from 'react-virtualized'
 
-
-// Create a DictionaryPopup component to show the images by grid view. Given that the images is from MVKImages, which is 1 cell 1 image.
+const LevelList = [
+    { level: 'MVK Dictionary', bg: TrapoziedBgGrayLeft },
+    { level: 'LHE Dictionary', bg: TrapoziedBgGray2 },
+]
+interface ImageDictionary {
+    name: string
+    source: string
+}
 
 const DictionaryPopup = () => {
     const columnCount = 5
-    const rowCount = Math.ceil(MVKImages.length / columnCount)
     const cellHeight = 150
-    const cellWidth = 200
-    const columnWidth = 200
-    const rowHeight = 200
 
-    const Cell = ({ columnIndex, rowIndex, style }: {
+    const [isMVKImages, setIsMVKImages] = useState(0)
+
+    const Cell = ({ columnIndex, rowIndex, style, images }: {
         columnIndex: number
         rowIndex: number
         style: React.CSSProperties
+        images: ImageDictionary[]
     }) => {
         const index = rowIndex * columnCount + columnIndex
-        if (index >= MVKImages.length) return null
+        if (index >= images.length) return null
 
-        const data = MVKImages[index]
+        const data = images[index]
         return (
             <Box sx={{
                 ...style,
@@ -34,9 +43,8 @@ const DictionaryPopup = () => {
                 justifyContent: 'center',
                 alignItems: 'center',
                 backgroundColor: 'white',
-                // margin: '100px',
+                borderRadius: '6px',
             }}>
-                {/* <img src={data.source} alt={data.name} style={{ width: '80%', alignItems: 'center', justifyContent: 'center', display: 'flex' }} /> */}
                 <Box
                     sx={{
                         display: 'flex',
@@ -48,6 +56,7 @@ const DictionaryPopup = () => {
                         boxShadow: '2px 4px 4px rgba(0, 0, 0, 0.5)',
                         margin: '5px',
                         backgroundColor: 'lightgray',
+                        border: '1px solid black',
                     }}
                 >
                     <img src={data.source} alt={data.name} style={{ width: '80%' }} />
@@ -57,6 +66,14 @@ const DictionaryPopup = () => {
                 </Box>
             </Box>
         )
+    }
+
+    const handleTabClick = (index: number) => {
+        if (index === 0) {
+            setIsMVKImages(0)
+        } else {
+            setIsMVKImages(1)
+        }
     }
 
     return (
@@ -74,6 +91,7 @@ const DictionaryPopup = () => {
             alignItems: 'center',
             borderRadius: '6px',
             boxShadow: '2px 4px 4px rgba(0, 0, 0, 0.5)',
+            border: '1px solid black',
         }}>
             <Box sx={{
                 position: 'flex',
@@ -81,30 +99,108 @@ const DictionaryPopup = () => {
                 justifyContent: 'center',
                 alignItems: 'center',
             }}>
-                <h1 style={{marginTop: '15px', marginBottom: '-3    px'}}>Dictionary</h1>
+                <h1 style={{ margin: '0' }}>Dictionary</h1>
             </Box>
-            <AutoSizer>
-                {({ height, width }) => {
-                    const columnWidth = width / columnCount - 1.5
-                    const rowHeight = cellHeight + 10 // Making rows square by setting row height equal to column width
-                    const rowCount = Math.ceil(MVKImages.length / columnCount)
-
-                    return (
-                        <Grid
-                            columnCount={columnCount}
-                            columnWidth={columnWidth}
-                            height={height}
-                            rowCount={rowCount}
-                            rowHeight={rowHeight}
-                            width={width}
-                            overscanRowCount={7}
-                            style={{ display: 'flex', justifyContent: 'flex-start', alignItems: 'flex-start' }}
-                        >
-                            {Cell}
-                        </Grid>
-                    )
+            <Box
+                sx={{
+                    display: 'flex',
+                    justifyContent: 'flex-start',
+                    position: 'fixed',
+                    marginBottom: '-1.5px',
+                    // paddingTop: '15px',
+                    paddingLeft: '15px',
+                    width: '78%',
+                    height: '78%',
+                    flexDirection: 'column',
+                    // backgroundColor: 'lightgray',
                 }}
-            </AutoSizer>
+            >
+                <Box>
+                    {LevelList.map((item, index) => (
+                        <button
+                            key={item.level}
+                            type="button"
+                            className={`font-base grid-tab text-gray ${index === isMVKImages ? 'active' : ''}`}
+                            style={{
+                                paddingTop: '0.375rem',
+                                paddingBottom: '0.375rem',
+                                width: '197px',
+                                backgroundImage: `url(${item.bg})`,
+                                zIndex: 90 - index * 10,
+                                border: 'none',  
+                                backgroundColor: 'transparent',
+                                marginLeft: `${index !== 0 && '-20px'}`,
+                                position: 'relative',
+                                height: '30px',
+                            }}
+                            onClick={() => handleTabClick(index)}
+                        >
+                            {item.level}
+                        </button>
+                    ))}
+                </Box>
+                {isMVKImages === 0 && (<AutoSizer>
+                    {({ height, width }) => {
+                        const columnWidth = width / columnCount - 1.5
+                        const rowHeight = cellHeight + 10
+                        const rowCount = Math.ceil(MVKImages.length / columnCount)
+
+                        return (
+                            <Grid
+                                columnCount={columnCount}
+                                columnWidth={columnWidth}
+                                height={height}
+                                rowCount={rowCount}
+                                rowHeight={rowHeight}
+                                width={width}
+                                overscanRowCount={7}
+                                style={{ display: 'flex', justifyContent: 'flex-start', alignItems: 'flex-start' }}
+                            >
+                                {({ columnIndex, rowIndex, style }) => (
+                                    <Cell
+                                        columnIndex={columnIndex}
+                                        rowIndex={rowIndex}
+                                        style={style}
+                                        images={MVKImages}
+                                    />
+                                )}
+                            </Grid>
+                        )
+                    }}
+                </AutoSizer>
+                )}
+                {isMVKImages !== 0 && (<AutoSizer>
+                    {({ height, width }) => {
+                        const columnWidth = width / columnCount - 1.5
+                        const rowHeight = cellHeight + 10
+                        const rowCount = Math.ceil(LHEImages.length / columnCount)
+
+                        return (
+                            <Grid
+                                columnCount={columnCount}
+                                columnWidth={columnWidth}
+                                height={height}
+                                rowCount={rowCount}
+                                rowHeight={rowHeight}
+                                width={width}
+                                overscanRowCount={7}
+                                style={{ display: 'flex', justifyContent: 'flex-start', alignItems: 'flex-start' }}
+                            >
+                                {({ columnIndex, rowIndex, style }) => (
+                                    <Cell
+                                        columnIndex={columnIndex}
+                                        rowIndex={rowIndex}
+                                        style={style}
+                                        images={LHEImages}
+                                    />
+                                )}
+                            </Grid>
+                        )
+                    }}
+                </AutoSizer>
+                )}
+            </Box>
+
         </Box>
     )
 
