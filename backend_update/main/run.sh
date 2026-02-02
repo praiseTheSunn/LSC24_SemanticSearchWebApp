4baf98f0f7d47de8f83ec72cf815eb9ae5ade2fc
minhquanBr2 <minhquanhole03@gmail.com>
2026-02-02 09:23:24 +0000

    modify env files, add dockerfile and docker-compose

--- backend_update/main/run.sh
+++ backend_update/main/run.sh
@@ -1,16 +1,11 @@
-source /root/miniconda3/etc/profile.d/conda.sh
-conda init
-conda activate main
-
-
 #!/usr/bin/env bash
 set -euo pipefail
 
-# Resolve repo root (works even if script is symlinked)
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-REPO_ROOT="$(cd "$SCRIPT_DIR/.." && pwd)"                    # Edit here when you move the script to another place
+source /opt/conda/etc/profile.d/conda.sh
+conda activate base
 
-export PYTHONPATH="$REPO_ROOT/libs"
+export PYTHONPATH=/app/libs
 
-
-python3 -m uvicorn main:app --host 0.0.0.0 --port 20721 --reload
+python -m uvicorn main:app \
+  --host 0.0.0.0 \
+  --port 20721
